6f543797157f92dab5e0d5fa199ff85995d0a439
jaewan <ljw6398@hanmail.net>
2021-05-09 15:38:48 +0900

    programmers start

---
+++ src/main/java/phoneketmon_1.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class phoneketmon_1 {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Solution {
+    public int solution(int[] nums) {
+        int result = 0;
+       HashSet<Integer> pk = new HashSet<Integer>();
+		for(int i=1; i<nums.length; i++) {
+			pk.add(nums[i]);
+		}
+        
+		if(pk.size() > nums.length/2) {
+			result = nums.length/2;
+		}
+        if(pk.size() <= nums.length/2){
+            result = pk.size();
+        }
+        return result;
+    }
+}
