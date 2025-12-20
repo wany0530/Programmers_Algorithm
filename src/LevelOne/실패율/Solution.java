a2b800ef814273f06086c880d466d1b79b6b8337
wany0530 <ljw6398@hanmail.net>
2025-12-20 23:45:26 +0900

    소수만들기 커밋

--- src/LevelOne/실패율/Solution.java
+++ src/LevelOne/실패율/Solution.java
@@ -1,7 +1,6 @@
 package LevelOne.실패율;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -10,17 +9,36 @@ public class Solution
 {
     public static void main(String[] args)
     {
-        int n = 5;
-        int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+        int n2 = 5;
+        double[] successAvgArr2 = new double[n2];
+        int[] stages2 = {2, 1, 2, 6, 2, 4, 3, 3};
 
-        List<Integer> list = Arrays.stream(stages).sorted().boxed().collect(Collectors.toList());
-        int base = 0;
-        for(Integer i : stages)
+        int length2 = stages2.length;
+
+        Map<Integer, Long> map = Arrays.stream(stages2).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        for(int i=1; i<=n2; i++)
         {
-            if(base != i)
-                base = i;
+            Long count = map.getOrDefault(i, 0L);
+            successAvgArr2[i-1] = length2 == 0 ? 0L : (double) count / length2;
 
-
+            length2 -= count;
         }
+
+        Integer[] indexArr = new Integer[n2];
+        for(int i=0; i<n2; i++)
+        {
+            indexArr[i] = i;
+        }
+
+        Arrays.sort(indexArr, (i, j) -> {
+            int cmp = Double.compare(successAvgArr2[j], successAvgArr2[i]);
+            if(cmp != 0)
+                return cmp;
+
+            return Integer.compare(i, j);
+        });
+
+        Arrays.stream(indexArr).mapToInt(i -> i + 1).forEach(System.out::println);
     }
 }
