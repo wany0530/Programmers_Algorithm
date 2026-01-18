b0fc3d74bfb2bed438cdf99e4523f99ec52d76d9
LeeJaeWan <ljw6398@naver.com>
2026-01-19 00:47:10 +0900

    키패드누르기 커밋

--- src/LevelOne/키패드누르기/Solution.java
+++ src/LevelOne/키패드누르기/Solution.java
@@ -19,7 +19,7 @@ public class Solution
 
     public static void main(String[] args)
     {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};Message Queue
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         String hand = "right";
 
         boolean rightHanded = "right".equals(hand);
